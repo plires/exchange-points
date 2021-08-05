e44dfe1f5efc6ccd6ab00d1f428a7a5b5ea19f09
Pablo Lires <pablolires@gmail.com>
2021-08-04 21:26:22 -0300

    Cambio de nombre de carpeta public a public_html

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,19 +11,20 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.sass('resources/sass/app.scss', 'public/admin/css')
-	.js('resources/js/admin/admin-users.js', 'public/js/admin')
-	.js('resources/js/admin/admin-products.js', 'public/js/admin')
-	.js('resources/js/admin/admin-points.js', 'public/js/admin')
-	.js('resources/js/admin/admin-points-assigned.js', 'public/js/admin')
-	.js('resources/js/admin/admin-points-exchanged.js', 'public/js/admin')
-	.js('resources/js/admin/admin-categories.js', 'public/js/admin')
-	.js('resources/js/admin/export-template-excel.js', 'public/js/admin')
-	.js('resources/js/admin/import-template-excel.js', 'public/js/admin')
-	.postCss('resources/css/admin/app.css', 'public/css/admin')
-	.js('resources/js/app/app.js', 'public/js/app')
-	.js('resources/js/app/catalog.js', 'public/js/app')
-	.js('resources/js/app/wallpaper.js', 'public/js/app')
-	.postCss('resources/css/app/app.css', 'public/css/app')
+mix.setPublicPath('public_html/');
+mix.sass('resources/sass/app.scss', 'public_html/admin/css')
+	.js('resources/js/admin/admin-users.js', 'public_html/js/admin')
+	.js('resources/js/admin/admin-products.js', 'public_html/js/admin')
+	.js('resources/js/admin/admin-points.js', 'public_html/js/admin')
+	.js('resources/js/admin/admin-points-assigned.js', 'public_html/js/admin')
+	.js('resources/js/admin/admin-points-exchanged.js', 'public_html/js/admin')
+	.js('resources/js/admin/admin-categories.js', 'public_html/js/admin')
+	.js('resources/js/admin/export-template-excel.js', 'public_html/js/admin')
+	.js('resources/js/admin/import-template-excel.js', 'public_html/js/admin')
+	.postCss('resources/css/admin/app.css', 'public_html/css/admin')
+	.js('resources/js/app/app.js', 'public_html/js/app')
+	.js('resources/js/app/catalog.js', 'public_html/js/app')
+	.js('resources/js/app/wallpaper.js', 'public_html/js/app')
+	.postCss('resources/css/app/app.css', 'public_html/css/app')
 
 mix.browserSync('http://puntos.test/');
