c4d08a65b4cad11aed2e8149862ef3a9d78bb551
Pablo Lires <pablolires@gmail.com>
2021-08-05 23:43:53 -0300

    Agregado de wallpapers y cambio de estructura en la vista

--- resources/views/app/wallpapers.blade.php
+++ resources/views/app/wallpapers.blade.php
@@ -73,6 +73,62 @@
 	    		<a href="{{ '/download-wallpaper/wallpaper-desktop-4' }}" class="btn btn-primary transition">Descargar</a>
 		    </div>
 
+		    <div class="col-md-6 text-center">
+		    	<a href="{{ '/download-wallpaper/wallpaper-desktop-5' }}" class="transition">
+	    			<img class="img-fluid transition" src="{{ asset('img/user/wallpapers/wallpaper-desktop-5.jpg') }}" alt="wallpaper desktop 5">
+	    		</a>
+	    		<a href="{{ '/download-wallpaper/wallpaper-desktop-5' }}" class="btn btn-primary transition">Descargar</a>
+		    </div>
+
+		    <div class="col-md-6 text-center">
+		    	<a href="{{ '/download-wallpaper/wallpaper-desktop-6' }}" class="transition">
+	    			<img class="img-fluid transition" src="{{ asset('img/user/wallpapers/wallpaper-desktop-6.jpg') }}" alt="wallpaper desktop 6">
+	    		</a>
+	    		<a href="{{ '/download-wallpaper/wallpaper-desktop-6' }}" class="btn btn-primary transition">Descargar</a>
+		    </div>
+
+		    <div class="col-md-6 text-center">
+		    	<a href="{{ '/download-wallpaper/wallpaper-desktop-7' }}" class="transition">
+	    			<img class="img-fluid transition" src="{{ asset('img/user/wallpapers/wallpaper-desktop-7.jpg') }}" alt="wallpaper desktop 7">
+	    		</a>
+	    		<a href="{{ '/download-wallpaper/wallpaper-desktop-7' }}" class="btn btn-primary transition">Descargar</a>
+		    </div>
+
+		    <div class="col-md-6 text-center">
+		    	<a href="{{ '/download-wallpaper/wallpaper-desktop-8' }}" class="transition">
+	    			<img class="img-fluid transition" src="{{ asset('img/user/wallpapers/wallpaper-desktop-8.jpg') }}" alt="wallpaper desktop 8">
+	    		</a>
+	    		<a href="{{ '/download-wallpaper/wallpaper-desktop-8' }}" class="btn btn-primary transition">Descargar</a>
+		    </div>
+
+		    <div class="col-md-6 text-center">
+		    	<a href="{{ '/download-wallpaper/wallpaper-desktop-9' }}" class="transition">
+	    			<img class="img-fluid transition" src="{{ asset('img/user/wallpapers/wallpaper-desktop-9.jpg') }}" alt="wallpaper desktop 9">
+	    		</a>
+	    		<a href="{{ '/download-wallpaper/wallpaper-desktop-9' }}" class="btn btn-primary transition">Descargar</a>
+		    </div>
+
+		    <div class="col-md-6 text-center">
+		    	<a href="{{ '/download-wallpaper/wallpaper-desktop-10' }}" class="transition">
+	    			<img class="img-fluid transition" src="{{ asset('img/user/wallpapers/wallpaper-desktop-10.jpg') }}" alt="wallpaper desktop 10">
+	    		</a>
+	    		<a href="{{ '/download-wallpaper/wallpaper-desktop-10' }}" class="btn btn-primary transition">Descargar</a>
+		    </div>
+
+		    <div class="col-md-6 text-center">
+		    	<a href="{{ '/download-wallpaper/wallpaper-desktop-11' }}" class="transition">
+	    			<img class="img-fluid transition" src="{{ asset('img/user/wallpapers/wallpaper-desktop-11.jpg') }}" alt="wallpaper desktop 11">
+	    		</a>
+	    		<a href="{{ '/download-wallpaper/wallpaper-desktop-11' }}" class="btn btn-primary transition">Descargar</a>
+		    </div>
+
+		    <div class="col-md-6 text-center">
+		    	<a href="{{ '/download-wallpaper/wallpaper-desktop-12' }}" class="transition">
+	    			<img class="img-fluid transition" src="{{ asset('img/user/wallpapers/wallpaper-desktop-12.jpg') }}" alt="wallpaper desktop 12">
+	    		</a>
+	    		<a href="{{ '/download-wallpaper/wallpaper-desktop-12' }}" class="btn btn-primary transition">Descargar</a>
+		    </div>
+
 			</div>
 	  </section>
 		<!-- Wallpapers Desktop end -->
@@ -88,46 +144,109 @@
 
 		  <div class="row">
 
-		  	<div class="col-6 col-md-4 text-center">
+		  	<div class="col-6 col-md-4 col-lg-3 text-center">
 		    	<a href="{{ '/download-wallpaper/wallpaper-mobile-1' }}" class="transition">
 	    			<img class="img-fluid transition" src="{{ asset('img/user/wallpapers/wallpaper-mobile-1.jpg') }}" alt="wallpaper mobile 1">
 	    		</a>
 	    		<a href="{{ '/download-wallpaper/wallpaper-mobile-1' }}" class="btn btn-primary transition">Descargar</a>
 		    </div>
 
-		    <div class="col-6 col-md-4 text-center">
+		    <div class="col-6 col-md-4 col-lg-3 text-center">
 		    	<a href="{{ '/download-wallpaper/wallpaper-mobile-2' }}" class="transition">
 	    			<img class="img-fluid transition" src="{{ asset('img/user/wallpapers/wallpaper-mobile-2.jpg') }}" alt="wallpaper mobile 2">
 	    		</a>
 	    		<a href="{{ '/download-wallpaper/wallpaper-mobile-2' }}" class="btn btn-primary transition">Descargar</a>
 		    </div>
 
-		    <div class="col-6 col-md-4 text-center">
+		    <div class="col-6 col-md-4 col-lg-3 text-center">
 		    	<a href="{{ '/download-wallpaper/wallpaper-mobile-3' }}" class="transition">
 	    			<img class="img-fluid transition" src="{{ asset('img/user/wallpapers/wallpaper-mobile-3.jpg') }}" alt="wallpaper mobile 3">
 	    		</a>
 	    		<a href="{{ '/download-wallpaper/wallpaper-mobile-3' }}" class="btn btn-primary transition">Descargar</a>
 		    </div>
 
-		    <div class="col-6 col-md-4 text-center">
+		    <div class="col-6 col-md-4 col-lg-3 text-center">
 		    	<a href="{{ '/download-wallpaper/wallpaper-mobile-4' }}" class="transition">
 	    			<img class="img-fluid transition" src="{{ asset('img/user/wallpapers/wallpaper-mobile-4.jpg') }}" alt="wallpaper mobile 4">
 	    		</a>
 	    		<a href="{{ '/download-wallpaper/wallpaper-mobile-4' }}" class="btn btn-primary transition">Descargar</a>
 		    </div>
 
-		    <div class="col-6 col-md-4 text-center">
+		    <div class="col-6 col-md-4 col-lg-3 text-center">
 		    	<a href="{{ '/download-wallpaper/wallpaper-mobile-5' }}" class="transition">
 	    			<img class="img-fluid transition" src="{{ asset('img/user/wallpapers/wallpaper-mobile-5.jpg') }}" alt="wallpaper mobile 5">
 	    		</a>
 	    		<a href="{{ '/download-wallpaper/wallpaper-mobile-5' }}" class="btn btn-primary transition">Descargar</a>
 		    </div>
 
-		    <div class="col-6 col-md-4 text-center">
+		    <div class="col-6 col-md-4 col-lg-3 text-center">
 		    	<a href="{{ '/download-wallpaper/wallpaper-mobile-6' }}" class="transition">
 	    			<img class="img-fluid transition" src="{{ asset('img/user/wallpapers/wallpaper-mobile-6.jpg') }}" alt="wallpaper mobile 6">
 	    		</a>
 	    		<a href="{{ '/download-wallpaper/wallpaper-mobile-6' }}" class="btn btn-primary transition">Descargar</a>
+		    </div>
+
+		    <div class="col-6 col-md-4 col-lg-3 text-center">
+		    	<a href="{{ '/download-wallpaper/wallpaper-mobile-7' }}" class="transition">
+	    			<img class="img-fluid transition" src="{{ asset('img/user/wallpapers/wallpaper-mobile-7.jpg') }}" alt="wallpaper mobile 7">
+	    		</a>
+	    		<a href="{{ '/download-wallpaper/wallpaper-mobile-7' }}" class="btn btn-primary transition">Descargar</a>
+		    </div>
+
+		    <div class="col-6 col-md-4 col-lg-3 text-center">
+		    	<a href="{{ '/download-wallpaper/wallpaper-mobile-8' }}" class="transition">
+	    			<img class="img-fluid transition" src="{{ asset('img/user/wallpapers/wallpaper-mobile-8.jpg') }}" alt="wallpaper mobile 8">
+	    		</a>
+	    		<a href="{{ '/download-wallpaper/wallpaper-mobile-8' }}" class="btn btn-primary transition">Descargar</a>
+		    </div>
+
+		    <div class="col-6 col-md-4 col-lg-3 text-center">
+		    	<a href="{{ '/download-wallpaper/wallpaper-mobile-9' }}" class="transition">
+	    			<img class="img-fluid transition" src="{{ asset('img/user/wallpapers/wallpaper-mobile-9.jpg') }}" alt="wallpaper mobile 9">
+	    		</a>
+	    		<a href="{{ '/download-wallpaper/wallpaper-mobile-9' }}" class="btn btn-primary transition">Descargar</a>
+		    </div>
+
+		    <div class="col-6 col-md-4 col-lg-3 text-center">
+		    	<a href="{{ '/download-wallpaper/wallpaper-mobile-10' }}" class="transition">
+	    			<img class="img-fluid transition" src="{{ asset('img/user/wallpapers/wallpaper-mobile-10.jpg') }}" alt="wallpaper mobile 10">
+	    		</a>
+	    		<a href="{{ '/download-wallpaper/wallpaper-mobile-10' }}" class="btn btn-primary transition">Descargar</a>
+		    </div>
+
+		    <div class="col-6 col-md-4 col-lg-3 text-center">
+		    	<a href="{{ '/download-wallpaper/wallpaper-mobile-11' }}" class="transition">
+	    			<img class="img-fluid transition" src="{{ asset('img/user/wallpapers/wallpaper-mobile-11.jpg') }}" alt="wallpaper mobile 11">
+	    		</a>
+	    		<a href="{{ '/download-wallpaper/wallpaper-mobile-11' }}" class="btn btn-primary transition">Descargar</a>
+		    </div>
+
+		    <div class="col-6 col-md-4 col-lg-3 text-center">
+		    	<a href="{{ '/download-wallpaper/wallpaper-mobile-12' }}" class="transition">
+	    			<img class="img-fluid transition" src="{{ asset('img/user/wallpapers/wallpaper-mobile-12.jpg') }}" alt="wallpaper mobile 12">
+	    		</a>
+	    		<a href="{{ '/download-wallpaper/wallpaper-mobile-12' }}" class="btn btn-primary transition">Descargar</a>
+		    </div>
+
+		    <div class="col-6 col-md-4 col-lg-3 text-center">
+		    	<a href="{{ '/download-wallpaper/wallpaper-mobile-13' }}" class="transition">
+	    			<img class="img-fluid transition" src="{{ asset('img/user/wallpapers/wallpaper-mobile-13.jpg') }}" alt="wallpaper mobile 13">
+	    		</a>
+	    		<a href="{{ '/download-wallpaper/wallpaper-mobile-13' }}" class="btn btn-primary transition">Descargar</a>
+		    </div>
+
+		    <div class="col-6 col-md-4 col-lg-3 text-center">
+		    	<a href="{{ '/download-wallpaper/wallpaper-mobile-14' }}" class="transition">
+	    			<img class="img-fluid transition" src="{{ asset('img/user/wallpapers/wallpaper-mobile-14.jpg') }}" alt="wallpaper mobile 14">
+	    		</a>
+	    		<a href="{{ '/download-wallpaper/wallpaper-mobile-14' }}" class="btn btn-primary transition">Descargar</a>
+		    </div>
+
+		    <div class="col-6 col-md-4 col-lg-3 text-center">
+		    	<a href="{{ '/download-wallpaper/wallpaper-mobile-15' }}" class="transition">
+	    			<img class="img-fluid transition" src="{{ asset('img/user/wallpapers/wallpaper-mobile-15.jpg') }}" alt="wallpaper mobile 15">
+	    		</a>
+	    		<a href="{{ '/download-wallpaper/wallpaper-mobile-15' }}" class="btn btn-primary transition">Descargar</a>
 		    </div>
 
 		  </div>
